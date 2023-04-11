06fd17c45d042d106be25f4e99896422492c17ff
Sabrina Tolay <sabrinatolay@gmail.com>
2023-04-11 14:02:21 -0300

    Inicia proyecto flask

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+def home():
+
+    return render_template('home-banking.html', saludo = persona1.saludo() )
+
+@app.route('/saludo')
 def home():#aca pongo que es lo que quiero que me devuelva cuando yo entro al home de mi pagina
 
     return render_template('index.html', saludo = persona1.saludo() ) # le digo que me renderize tal html con la informacion de la variable saludo
